7a0a142e93c33a5c9a27be2176d9333d0dcc05d6
Fernando Ibanez <fibanez84@gmail.com>
2023-01-20 12:38:59 +1100

    jest

--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,6 @@
+import seoData from 'content/seo-data.json';
 import { NextSeo } from 'next-seo';
-import { NEXT_SEO_DEFAULT } from 'next-seo-config';
 
 export default function Head(): JSX.Element {
-  return <NextSeo {...NEXT_SEO_DEFAULT} useAppDir={true} />;
+  return <NextSeo {...seoData} useAppDir={true} />;
 }
