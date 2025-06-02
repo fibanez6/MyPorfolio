08987e4a7069ee2e26cc32710adb32205600c60e
Fernando Ibanez <fernando.ibanez@bjss.com>
2025-06-02 10:05:45 +1000

    certs

--- content/profile-data.ts
+++ content/profile-data.ts
@@ -51,6 +51,26 @@ export const CERTIFICATES = [
     alt: 'AWS Certified Solutions Architect Associate (SOA-C02)',
     date: 'January 2025',
     link: 'https://cp.certmetrics.com/amazon/en/public/verify/credential/3a6bbc9abc3544b99a9c0e242df4e1c7'
+  },
+  {
+    title: 'Generative AI Essentials',
+    subtitle: 'AWS Partner',
+    description: 'Amazon Web Services (AWS)',
+    credentialId: 'be187051-c68d-445e-bb32-0456efa5f06b',
+    image: '/media/AWS-Partner-Generative-AI-Essentials.png',
+    alt: 'AWS Partner - Generative AI Essentials',
+    date: 'February 2024',
+    link: 'https://www.credly.com/badges/be187051-c68d-445e-bb32-0456efa5f06b/public_url'
+  },
+  {
+    title: 'Prompt Design in Vertex AI',
+    subtitle: 'Machine Learning & AI',
+    description: 'Google Cloud',
+    credentialId: '101df7db-226d-4d88-b829-abfe0617847b',
+    image: '/media/Google-Cloud-prompt-design-in-vertex-ai.png',
+    alt: 'Google Cloud - Machine Learning & AI - Prompt Design in Vertex AI',
+    date: 'June 2024',
+    link: 'https://www.credly.com/badges/101df7db-226d-4d88-b829-abfe0617847b/public_url'
   }
 ];
 
